1bdec538c860053ec4e6e1945c4786975f817c0d
apshaiTerp <apshaiTerp@yahoo.com>
2016-11-16 15:00:02 -0600

    Another attempt

--- src/main/java/com/ac/umkc/rest/SparkRESTApplication.java
+++ src/main/java/com/ac/umkc/rest/SparkRESTApplication.java
@@ -4,25 +4,16 @@ import javax.annotation.PreDestroy;
 
 import org.apache.spark.sql.SparkSession;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author AC010168
  *
  */
-@ComponentScan
-@EnableAutoConfiguration
-public class SparkRESTApplication extends SpringBootServletInitializer {
+@SpringBootApplication
+public class SparkRESTApplication {
 
   public static SparkSession sparkSession = null;
-  
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    return application.sources(SparkRESTApplication.class);
-  }
   
   /** Main method, which is starting point for service using Spring launcher */
   public static void main(String[] args) {
