a91cb6ce958ca5a927b7d78e0b5e7421672b7fb1
apshaiTerp <apshaiTerp@yahoo.com>
2016-11-29 11:13:38 -0600

    Checking in some method increases and improvements

--- src/main/java/com/ac/umkc/rest/controller/QueryFiveController.java
+++ src/main/java/com/ac/umkc/rest/controller/QueryFiveController.java
@@ -2,6 +2,7 @@ package com.ac.umkc.rest.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ac.umkc.rest.data.SimpleErrorData;
@@ -24,7 +25,13 @@ public class QueryFiveController implements Serializable {
    * @return
    */
   @RequestMapping(method = RequestMethod.GET, produces="application/json;charset=UTF-8")
-  public Object getQuery5() {
+  public Object getQuery5(@RequestParam(value="startdate") String startDate,
+                          @RequestParam(value="enddate") String endDate) {
+    if ((startDate == null) || (startDate.trim().length() == 0))
+      return new SimpleErrorData("Invalid Request", "No startdate was provided.");
+    if ((endDate == null) || (endDate.trim().length() == 0))
+      return new SimpleErrorData("Invalid Request", "No enddate was provided.");
+    
     System.out.println ("*************************************************************************");
     System.out.println ("***************************  Execute Query 5  ***************************");
     System.out.println ("*************************************************************************");
--- src/main/java/com/ac/umkc/rest/controller/QueryTwoController.java
+++ src/main/java/com/ac/umkc/rest/controller/QueryTwoController.java
@@ -9,6 +9,7 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import scala.Serializable;
@@ -29,31 +30,35 @@ public class QueryTwoController implements Serializable {
    * @return
    */
   @RequestMapping(method = RequestMethod.GET, produces="application/json;charset=UTF-8")
-  public Object getQuery2() {
+  public Object getQuery2(@RequestParam(value="startdate") String startDate,
+                          @RequestParam(value="enddate") String endDate) {
+    if ((startDate == null) || (startDate.trim().length() == 0))
+      return new SimpleErrorData("Invalid Request", "No startdate was provided.");
+    if ((endDate == null) || (endDate.trim().length() == 0))
+      return new SimpleErrorData("Invalid Request", "No enddate was provided.");
+
     System.out.println ("*************************************************************************");
     System.out.println ("***************************  Execute Query 2  ***************************");
     System.out.println ("*************************************************************************");
     
     //TODO - More Stuff
+    boolean readFromCache = false;
+    String dynamicPath = "/proj3/query2/" + startDate + "/" + endDate;
     try {
       Configuration hdfsConfiguration = new Configuration();
-      hdfsConfiguration.set("fs.default.name", "hdfs://localhost:9000");
+      hdfsConfiguration.set("fs.defaultFS", "hdfs://localhost:9000");
       FileSystem hdfs                 = FileSystem.get(hdfsConfiguration);
       
-      
-      Path knownFile = new Path("/proj3/userdata.txt");
-      Path localFile = new Path("/proj3/query2/2016/2017");
-      if (hdfs.exists(knownFile))
+      Path checkFile = new Path(dynamicPath);
+      if (hdfs.exists(checkFile)) {
         System.out.println ("I found the known file!");
-      else System.out.println ("I could not find the known file.  Boo!");
-      
-      if (hdfs.exists(localFile))
-        System.out.println ("This path exists!");
-      else System.out.println ("This path does not exist!  We've got to run the query ad-hoc.  :-(");
-
+        readFromCache = true;
+      } else System.out.println ("I could not find the known file.  Boo!");
     } catch (IOException e) {
       e.printStackTrace();
     }
+    
+    System.out.println ("Read from Cache: " + readFromCache);
 
     System.out.println ("-------------------------------------------------------------------------");
     System.out.println ("-----------------------------  End Query 2  -----------------------------");
