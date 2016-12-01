701094e36dbe99b1cca9655c8ed1074d3ec30ef4
apshaiTerp <apshaiTerp@yahoo.com>
2016-12-01 13:42:48 -0600

    Trying a new option to REST call

---
+++ src/main/java/com/ac/umkc/rest/controller/QueryFiveSupportController.java
@@ -0,0 +1,44 @@
+package com.ac.umkc.rest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ac.umkc.rest.data.SimpleErrorData;
+import com.ac.umkc.spark.util.TwitterCall;
+
+import scala.Serializable;
+
+/**
+ * @author AC010168
+ *
+ */
+@RestController
+@RequestMapping("/query5support")
+public class QueryFiveSupportController implements Serializable {
+
+  /** Added because it needs it */
+  private static final long serialVersionUID = 180014941987416042L;
+  
+  @RequestMapping(method = RequestMethod.GET, produces="application/json;charset=UTF-8")
+  public Object getQuery5(@RequestParam(value="name") String userName,
+                          @RequestParam(value="id", defaultValue="0") long statusID) {
+    if ((userName == null) || (userName.trim().length() == 0))
+      return new SimpleErrorData("Invalid Request", "No name was provided.");
+    if (statusID <= 0)
+      return new SimpleErrorData("Invalid Request", "No valid id was provided.");
+   
+    String requestURL = "https://publish.twitter.com/oembed?url=https://twitter.com/" + userName + 
+        "/status/" + statusID;
+    String result = "";
+    try {
+      result = TwitterCall.getEmbedBody(requestURL);
+    } catch (Throwable t) {
+      t.printStackTrace();
+      return new SimpleErrorData("REST API Failure", "Oops: " + t.getMessage());
+    }
+
+    return "{\"result\": \"" + result + "\"}";
+  }
+}
